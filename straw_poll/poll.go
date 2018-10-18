7696e3b3564784491b88ea57e1b789946f245e74
djdoeslinux <dj@example.com>
2018-10-17 18:37:27 -0700

    Strawpoll roughout

--- straw_poll/poll.go
+++ straw_poll/poll.go
@@ -5,6 +5,11 @@
 
 package straw_poll
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 // This will implement a straw poll with a timeout or vote threshold.
 // Can be used for long running polls (with names) or ad-hoc short-lived polls
 
@@ -13,3 +18,28 @@ var Models []interface{}
 func init() {
 	//Models = append(Models, &Tracker{})
 }
+
+type StrawPoll struct {
+	gorm.Model
+	ChannelID       uint
+	Name            string
+	Options         []PollOption
+	RestrictedRoles []VoteRestriction
+	VoteExpiresAt   time.Time
+}
+
+type VoteRestriction struct {
+	gorm.Model
+	StrawPollID uint
+	Role        string
+}
+
+type PollOption struct {
+	gorm.Model
+	StrawPollID uint
+}
+
+//!newPoll <name> --option "10" --option "20" --option "30" --duration --start --voting-role
+//!runPoll <name> --duration
+//!modifyPoll <name> [options]
+//!resetPoll <name>
